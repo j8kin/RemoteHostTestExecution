33429b7bc07047f45788dc1e8a9eb818f3891525
j8kin <emk161278@gmail.com>
2019-05-02 17:01:59 +0300

    add simple scenario with trying to upload file with OsTestFramwork an PsExec.exe

--- RemoteHostTestExecution/UnitTest1.cs
+++ RemoteHostTestExecution/UnitTest1.cs
@@ -1,14 +1,30 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using JetBrains.OsTestFramework;
+using NUnit.Framework;
+
 
 namespace RemoteHostTestExecution
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        private readonly string _assemblyDirectory = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().EscapedCodeBase).AbsolutePath);
+
+
+        private const string Ip = "192.168.0.84";
+        private const string UserName = "IEUser";
+        private const string Password = "Passw0rd!";
+
+        [Test()]
         public void TestMethod1()
         {
+
+            var operatingSystem = new RemoteEnvironment(Ip, UserName, Password, Path.Combine(_assemblyDirectory, @"..\..\tools\PsExec.exe"));
+            operatingSystem.CopyFileFromGuestToHost(@"C:\Downloads\7z920-x64.msi", @"C:\Temp");
+            //Console.WriteLine(operatingSystem.GuestEnvironmentVariables.Count);
+
         }
     }
 }
