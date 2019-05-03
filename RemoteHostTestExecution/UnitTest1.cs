ab524065d7ac0b956f573629fb7c7d55cc7b110f
j8kin <emk161278@gmail.com>
2019-05-03 12:14:19 +0300

    Add unzip unit test on Guest PC

--- RemoteHostTestExecution/UnitTest1.cs
+++ RemoteHostTestExecution/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.OsTestFramework;
+﻿using System;
+using JetBrains.OsTestFramework;
 using NUnit.Framework;
 
 
@@ -15,10 +16,20 @@ namespace RemoteHostTestExecution
         [Test]
         public void TestMethod1()
         {
-            var operatingSystem = new RemoteEnvironment(Ip, UserName, Password, @"c:\Users\\Eugene\source\repos\PSTools\PsExec.exe");
-            operatingSystem.CopyFileFromHostToGuest(@"c:\Users\Eugene\source\repos\LeetCodeZigZag\ZigZagTest\bin\Debug\ZigZagConversion.zip", @"C:\_work");
-            //Console.WriteLine(operatingSystem.GuestEnvironmentVariables.Count);
+            using (var operatingSystem = new RemoteEnvironment(Ip, UserName, Password,
+                @"c:\Users\\Eugene\source\repos\PSTools\PsExec.exe"))
+            {
 
+                // Copy nUnit test to Guest PC
+                operatingSystem.CopyFileFromHostToGuest(
+                    @"c:\Users\Eugene\source\repos\LeetCodeZigZag\ZigZagTest\bin\Debug\ZigZagConversion.zip",
+                    @"C:\_work");
+                // Unzip nUnit test on Guest PC
+                operatingSystem.WindowsShellInstance.DetachElevatedCommandInGuestNoRemoteOutput(
+                    @"7z.exe x -aoa -oc:\_work\Output c:\_work\ZigZagConversion.zip", TimeSpan.FromSeconds(1));
+                // Execute nUnit test on Guest PC
+                //Console.WriteLine(operatingSystem.GuestEnvironmentVariables.Count);
+            }
         }
     }
 }
