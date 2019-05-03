d5190ae0b0e4f3fe6d3e8ad18e3d254b97cfc9e1
j8kin <emk161278@gmail.com>
2019-05-03 17:51:27 +0300

    execute nunit test on remote host and download result

--- RemoteHostTestExecution/UnitTest1.cs
+++ RemoteHostTestExecution/UnitTest1.cs
@@ -13,22 +13,33 @@ namespace RemoteHostTestExecution
         private const string UserName = "nunittest";
         private const string Password = "123456";
 
+        private const string PathToNUnitTest = @"c:\Users\Eugene\source\repos\LeetCodeZigZag\ZigZagTest\bin\Debug";
+
         [Test]
         public void TestMethod1()
         {
             using (var operatingSystem = new RemoteEnvironment(Ip, UserName, Password,
-                @"c:\Users\\Eugene\source\repos\PSTools\PsExec.exe"))
+                @"c:\Users\Eugene\source\repos\PSTools\PsExec.exe"))
             {
 
                 // Copy nUnit test to Guest PC
                 operatingSystem.CopyFileFromHostToGuest(
-                    @"c:\Users\Eugene\source\repos\LeetCodeZigZag\ZigZagTest\bin\Debug\ZigZagConversion.zip",
+                    PathToNUnitTest+@"\ZigZagConversion.zip",
                     @"C:\_work");
                 // Unzip nUnit test on Guest PC
                 operatingSystem.WindowsShellInstance.DetachElevatedCommandInGuestNoRemoteOutput(
                     @"7z.exe x -aoa -oc:\_work\Output c:\_work\ZigZagConversion.zip", TimeSpan.FromSeconds(1));
                 // Execute nUnit test on Guest PC
-                //Console.WriteLine(operatingSystem.GuestEnvironmentVariables.Count);
+                operatingSystem.WindowsShellInstance.DetachElevatedCommandInGuestNoRemoteOutput(
+                    @"nunit3-console --work=c:\_work\Output c:\_work\Output\ZigZagTest.dll", TimeSpan.FromSeconds(1));
+                
+                // Verify that nunit execution test result (TestResult.xml) exist in "c:\_work\Output"
+                Assert.True(operatingSystem.FileExistsInGuest(@"c:\_work\Output\TestResult.xml"));
+
+                // Copy Test Result from Guest to Host
+                operatingSystem.CopyFileFromGuestToHost(
+                    @"c:\_work\Output\TestResult.xml",
+                    PathToNUnitTest);
             }
         }
     }
